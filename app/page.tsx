ebdf53f7561c417fc80a66bcb5ba1460e28bf6b2
OuwePiet <94792393+OuwePiet@users.noreply.github.com>
2026-08-03 12:06:39 +0200

    Add button to buy NFT on Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 export default function Home() {
   return (
     <main>
@@ -7,7 +8,18 @@ export default function Home() {
       <button onClick={() => window.open("https://nftz.me/nft/000929e4490e3f744a7c889738d3aef52397ac72af906e5cd473bde710b49111")}>
         Buy NFT
       </button>
-/page.tsx   ✅
+
     </main>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
