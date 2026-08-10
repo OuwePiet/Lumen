cca6d331e4dac42f55b9c1d0467156ad244c4b18
OuwePiet <94792393+OuwePiet@users.noreply.github.com>
2026-08-10 12:06:56 +0200

    Show read-only NFT collection on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import NFTView from "./nft-view"
-
-const EXAMPLE_POST_HASH =
-  "000929e4490e3f744a7c889738d3aef52397ac72af906e5cd473bde710b49111"
+import NFTGrid from "./nft-grid"
 
 export const dynamic = "force-dynamic"
 
 export default function Home() {
-  return <NFTView postHash={EXAMPLE_POST_HASH} />
+  return <NFTGrid />
 }
