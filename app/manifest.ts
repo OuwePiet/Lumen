0c3e94e7256fe3a31efa40a1fb984e2adf71e14a
OuwePiet <94792393+OuwePiet@users.noreply.github.com>
2026-09-19 19:37:19 +0200

    Use NFT watermark for VIA app manifest icon (#890)

--- app/manifest.ts
+++ app/manifest.ts
@@ -14,13 +14,13 @@ export default function manifest(): MetadataRoute.Manifest {
     categories: ["social", "entertainment", "art"],
     icons: [
       {
-        src: "/icon.svg",
+        src: "/via-leaf.svg",
         sizes: "any",
         type: "image/svg+xml",
         purpose: "any",
       },
       {
-        src: "/icon.svg",
+        src: "/via-leaf.svg",
         sizes: "any",
         type: "image/svg+xml",
         purpose: "maskable",
