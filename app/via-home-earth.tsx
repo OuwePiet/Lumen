e2c439a483aa345f436b033765a3991a5e0efd6b
OuwePiet <94792393+OuwePiet@users.noreply.github.com>
2026-09-24 11:07:07 +0200

    Adjust iPhone Earth diameter only

--- app/via-home-earth.tsx
+++ app/via-home-earth.tsx
@@ -103,8 +103,8 @@ export default function ViaHomeEarth() {
         @media (max-width: 900px) {
           .via-nasa-earth-poster,
           .via-nasa-earth-video {
-            width: 216%;
-            height: 216%;
+            width: 404%;
+            height: 404%;
             left: 47%;
             top: -22%;
             transform: translate(-50%, calc(-60vh + 117px));
