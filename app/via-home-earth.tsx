febf0383239e9322d6f9217eb668e0e6b2490a18
OuwePiet <94792393+OuwePiet@users.noreply.github.com>
2026-09-25 10:16:55 +0200

    Homepage: move iPhone Earth up one button

--- app/via-home-earth.tsx
+++ app/via-home-earth.tsx
@@ -107,7 +107,7 @@ export default function ViaHomeEarth() {
             height: 216%;
             left: 47%;
             top: -22%;
-            transform: translate(-50%, calc(-60vh + 251px)) scale(1.87);
+            transform: translate(-50%, calc(-60vh + 207px)) scale(1.87);
             image-rendering: auto;
             filter: brightness(1.42) contrast(1.12) saturate(1.16);
           }
